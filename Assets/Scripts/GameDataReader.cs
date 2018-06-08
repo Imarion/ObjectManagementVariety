578c5834e9c8e583007bfcdba1bf858174bf7907
emr <renquin@awtce.be>
2018-06-08 14:00:08 +0200

    Material + color + instance shader. + save MAterial and color.

--- Assets/Scripts/GameDataReader.cs
+++ Assets/Scripts/GameDataReader.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class GameDataReader {
 
 	BinaryReader reader;
+	public int Version { get; }
 
-	public GameDataReader (BinaryReader reader) {
+	public GameDataReader (BinaryReader reader, int version) {
 		this.reader = reader;
+		this.Version = version;
 	}
 
 	public float ReadFloat () {
@@ -33,4 +35,14 @@ public class GameDataReader {
 		value.z = reader.ReadSingle();
 		return value;
 	}
+
+	public Color ReadColor () {
+		Color value;
+		value.r = reader.ReadSingle();
+		value.g = reader.ReadSingle();
+		value.b = reader.ReadSingle();
+		value.a = reader.ReadSingle();
+		return value;
+	}
+
 }
--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Security.Cryptography;
+using UnityEngine.AI;
 
 public class Shape : PersistableObject {
 	int shapeId = int.MinValue;
+	public int MaterialId { get; private set; }
+	Color color;
+	MeshRenderer meshRenderer;
+
+	static int colorPropertyid = Shader.PropertyToID("_Color");
+	static MaterialPropertyBlock sharedPropertyBlock;
 
 	public int ShapeId {
 		get {
@@ -16,6 +24,38 @@ public class Shape : PersistableObject {
 				Debug.LogError ("Not allowed to change shapeId");
 			}
 		}
+	}
+
+	void Awake()
+	{
+		meshRenderer = GetComponent<MeshRenderer> ();
+	}
+
+	public void SetMaterial (Material material, int materialId) {
+		meshRenderer.material = material;
+		MaterialId = materialId;
+	}
+
+	public void SetColor (Color color) {
+		this.color = color;
+
+		if (sharedPropertyBlock == null) 
+		{
+			sharedPropertyBlock = new MaterialPropertyBlock ();
+		}
+		sharedPropertyBlock.SetColor (colorPropertyid, color);
+		meshRenderer.SetPropertyBlock (sharedPropertyBlock);
+
+	}
+
+	public override void Save (GameDataWriter writer) {
+		base.Save(writer);
+		writer.Write(color);
+	}
+
+	public override void Load (GameDataReader reader) {
+		base.Load(reader);
+		SetColor(reader.Version > 0 ? reader.ReadColor() : Color.white);
 	}
 
 }
